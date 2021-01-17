066f1defc359504180757f9114436dedde6b715a
Przemek <p.plackowski@gmail.com>
2021-01-17 11:51:38 +0100

    Change message for example of test

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -2,6 +2,6 @@ import { Main } from '../src/main';
 
 describe('index.ts example test', () => {
   it('should return short sentence with my name', () => {
-    expect(Main.hello('Przemyslaw')).toBe('Hi, Przemyslaw');
+    expect(Main.hello('Joe Doe')).toBe('Hi, Joe Doe');
   });
 });
